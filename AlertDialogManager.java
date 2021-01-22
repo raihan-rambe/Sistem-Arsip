5e6e6c33d010b89bf71285bfceb93fbefdd6ad31
Raihani-rambe <48796531+raihan-rambe@users.noreply.github.com>
2021-01-22 13:01:19 +0700

    Add files via upload

---
+++ AlertDialogManager.java
@@ -0,0 +1,27 @@
+package com.example.rensposi.Adapter;
+
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+import com.example.rensposi.R;
+
+public class AlertDialogManager {
+    public void showAlertDialog(Context context, String title, String message, Boolean status) {
+
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+
+        if (status != null)
+            alertDialog.setIcon((status) ? R.drawable.baground : R.drawable.market);
+        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        alertDialog.show();
+    }
+}
+
+
